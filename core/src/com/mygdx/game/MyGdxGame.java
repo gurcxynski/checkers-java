031b81ac1d83ec3ccdd44a8771f7f7a0281c8fc2
gurcxynski <woj.gorczynski@gmail.com>
2023-10-26 15:43:46 +0200

    Shifted updating and drawing to state machine

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,65 +1,43 @@
 package com.mygdx.game;
 
+import java.util.HashMap;
+
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture red;
-	Texture white;
-	Texture redKing;
-	Texture whiteKing;
-	Texture boardTexture;
 	StateMachine machine;
-	final boolean whitePlayer = true;
-	Piece held;
-	int cellX;
-	int cellY;
-
+	HashMap<String, Texture> textures;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		red = new Texture("red.png");
-		white = new Texture("white.png");
-		redKing = new Texture("redKing.png");
-		whiteKing = new Texture("whiteKing.png");
-		boardTexture = new Texture("board.png");
+
+		textures = new HashMap<String, Texture>();
+		textures.put("red", new Texture("red.png"));
+		textures.put("white", new Texture("white.png"));
+		textures.put("rking", new Texture("redKing.png"));
+		textures.put("wking", new Texture("whiteKing.png"));
+		textures.put("board", new Texture("board.png"));
+
 		machine = new StateMachine();
-		machine.NewGame();
+		machine.NewGame(true);
 	}
 
 	@Override
 	public void render () {
 
-		if (Gdx.input.justTouched()) {
-			int x = Gdx.input.getX() / 60;
-			int y = 7 - Gdx.input.getY() / 60;
-			String to = Helpers.convertCords(x, y);
-			if (Globals.board.getField(x, y) != null) { 
-				held = Globals.board.getField(x, y);
-				System.out.println("Held " + held.getX() + " " + held.getY());
-			}
-			else if (held != null) machine.ExecuteMove(new Move(Helpers.convertCords(held.getX(), held.getY()) + to, machine.WhiteToMove));
-		}
-
 		batch.begin();
 
-		ScreenUtils.clear(Color.SKY);
-		batch.draw(boardTexture, 0, 0, 0, 0, 480, 480, 1, 1, 0, 0, 0, 240, 240, false, !whitePlayer);
-		for (Piece piece : Globals.board.pieces) {
-			batch.draw(piece.isWhite() ? (piece.isKing() ? whiteKing : white) : (piece.isKing() ? redKing : red), piece.getX() * 60, whitePlayer ? piece.getY() * 60 : 420 - piece.getY() * 60, 60, 60);
-		}
+		machine.Update();
+		machine.Draw(batch, textures);
+		
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		red.dispose();
-		white.dispose();
 	}
 }
--- core/src/com/mygdx/game/StateMachine.java
+++ core/src/com/mygdx/game/StateMachine.java
@@ -1,29 +1,48 @@
 package com.mygdx.game;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StateMachine {
     public enum GameState {
+        MENU,
         MOVING,
         AWATING_ENEMY_MOVE,
         WHITE_WON,
         BLACK_WON,
-        DRAW,
-
+        DRAW
     }
-    boolean WhiteToMove = true;
+    GameState state = GameState.MOVING;
+
+
+    Scene scene;
     ArrayList<Move> moveList;
-    public void NewGame() {
+    public void Update() {
+        Move move = scene.Update();
+        if (move != null) ExecuteMove(move);
+    }
+    public void Draw(SpriteBatch batch, HashMap<String, Texture> textures ) {
+
+        if (state != GameState.MENU) {
+            scene.Draw(batch, textures);
+            return;
+        }
+        // Draw menu
+    }
+    public void NewGame(boolean white) {
+        scene = new Scene(white);
         Globals.board = new Board();
         Globals.board.Initialize();
         moveList = new ArrayList<Move>();
     }
     public boolean ExecuteMove(Move move) {
         if (Globals.board.ExecuteMove(move)) {
-            if ((WhiteToMove && move.to[1] == 7) || (!WhiteToMove && move.to[1] == 0)) {
+            if ((scene.whitePlayer && move.to[1] == 7) || (!scene.whitePlayer && move.to[1] == 0)) {
                 Globals.board.getField(move.to).kingMe();
             }
-            WhiteToMove = !WhiteToMove;
             moveList.add(move);
             return true;
         }
