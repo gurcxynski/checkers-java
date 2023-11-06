923d9f231e3e74442c11e24b606465d677da3039
Wazks <tomasz.cieplinski@onet.pl>
2023-11-06 18:46:41 +0100

    Automatically load all textures from assets folder

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,46 +1,38 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mygdx.game.StateMachine.GameState;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-    public static Skin skin;
-    public static StateMachine machine;
+	public static Skin skin;
+	public static StateMachine machine;
 
 	@Override
 	public void create() {
-
 		skin = new Skin();
-		loadTexture("black");
-		loadTexture("white");
-		loadTexture("blackking");
-		loadTexture("whiteking");
-		loadTexture("playsolo");
-		loadTexture("playonline");
-		loadTexture("quit");
-		loadTexture("join");
-		loadTexture("white_button");
-		loadTexture("black_button");
-		loadTexture("dark_tile");
-		loadTexture("light_tile");
-		loadTexture("youwon");
-		loadTexture("youlost");
-		loadTexture("whitewon");
-		loadTexture("blackwon");
-
+		loadTextures();
 		machine = new StateMachine();
 	}
 
 	@Override
 	public void render() {
-		if (machine.state == GameState.AWATING_NETWORK) machine.check_for_incoming();
+		if (machine.state == GameState.AWATING_NETWORK)
+			machine.check_for_incoming();
 		machine.draw();
 	}
 
-	void loadTexture(String name) {
-		skin.add(name, new Texture(name + ".png"));
+	void loadTextures() {
+		FileHandle[] files = Gdx.files.internal("./").list();
+
+		for (FileHandle file : files) {
+			String textureName = file.nameWithoutExtension();
+			skin.add(textureName, new Texture(file.name()));
+		}
+
 	}
 }
