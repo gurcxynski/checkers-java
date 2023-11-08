4dcafb2816368c35adee508e2a791827bee90be7
gurcxynski <woj.gorczynski@gmail.com>
2023-11-08 11:27:52 +0100

    Reduced amount of code for check buttons

--- core/src/com/mygdx/game/ui/HostMenu.java
+++ core/src/com/mygdx/game/ui/HostMenu.java
@@ -1,49 +1,19 @@
 package com.mygdx.game.ui;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.mygdx.game.Game;
 
-public class HostMenu extends Menu {    
-    boolean white_selected = true;
+public class HostMenu extends Menu {
     public HostMenu() {
-        addActor(new MyButtonCheck(100, 650, "white_unchecked", "white_checked", white_selected, new InputListener() {
-	        public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-            
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-                if (x > 0 && x < getWidth() && y > 0 && y < getHeight()) {
-                    for (Actor actor : Game.machine.activeStage.getActors()) {
-                        if (actor instanceof MyButtonCheck) {
-                            ((MyButtonCheck) actor).setChecked(false);
-                        }
-                    }
-                    ((MyButtonCheck) event.getTarget()).setChecked(true);
-                    }
-                    white_selected = true;
-                }
-        }));
-        addActor(new MyButtonCheck(300, 650, "black_unchecked", "black_checked", !white_selected, new InputListener() {
-	        public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-            
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-                if (x > 0 && x < getWidth() && y > 0 && y < getHeight()) {
-                    for (Actor actor : Game.machine.activeStage.getActors()) {
-                        if (actor instanceof MyButtonCheck) {
-                            ((MyButtonCheck) actor).setChecked(false);
-                        }
-                    }
-                    ((MyButtonCheck) event.getTarget()).setChecked(true);
-                    }
-                    white_selected = false;
-                }
-        }));
+        final MyButtonCheck white = new MyButtonCheck(100, 650, "white_unchecked", "white_checked", true, new MyListener());
+        final MyButtonCheck black = new MyButtonCheck(300, 650, "black_unchecked", "black_checked", false, new MyListener());
+
+        white.disable.add(black);
+        black.disable.add(white);
+
+        addActor(white);
+        addActor(black);
         addActor(new MyButton(100, 350, "host", "light_tile", new MyListener() {
-            public void onClick() { Game.machine.hostOnlineGame(white_selected); }
+            public void onClick() { Game.machine.hostOnlineGame(white.isChecked()); }
         }));
         addActor(new MyButton(100, 50, "back", "light_tile", new MyListener() {
 	        public void onClick() { Game.machine.toMenu(new StartMenu()); }
--- core/src/com/mygdx/game/ui/MyButtonCheck.java
+++ core/src/com/mygdx/game/ui/MyButtonCheck.java
@@ -1,12 +1,15 @@
 package com.mygdx.game.ui;
 
+import java.util.LinkedList;
+
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.mygdx.game.Game;
 
 public class MyButtonCheck extends Button {
-    public MyButtonCheck(int x, int y, String texture_up, String texture_checked, boolean startChecked, InputListener listener) {
+    LinkedList<MyButtonCheck> disable = new LinkedList<MyButtonCheck>();
+    public MyButtonCheck(int x, int y, String texture_up, String texture_checked, boolean startChecked, final MyListener listener) {
         Drawable up = Game.skin.getDrawable(texture_up);
         Drawable checked = Game.skin.getDrawable(texture_checked);
         setBounds(x, y, up.getMinWidth(), up.getMinHeight());
@@ -15,6 +18,20 @@ public class MyButtonCheck extends Button {
         style.up = up;
         setChecked(startChecked);
         setStyle(style);
-        addListener(listener);
+        InputListener inputListener = new InputListener() {
+            public boolean touchDown(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+
+            public void touchUp(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y, int pointer, int button) {
+                if (x > 0 && x < getWidth() && y > 0 && y < getHeight()) {
+                    listener.onClick();
+                    for (MyButtonCheck other : disable) {
+                        other.setChecked(false);
+                    }
+                }
+            }
+        };
+        addListener(inputListener);
     }
 }
